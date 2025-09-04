f83c752478d3c87653e015128fe814b80311b099
jalcala5-cloud <jalcala5@mail.pima.edu>
2025-09-04 14:35:00 -0700

    Add files via upload

---
+++ Freddy.pyi
@@ -0,0 +1,176 @@
+
+# Just print a cat face using text (ASCII art)
+cat = r"""
+ /\_/\  
+( o.o )
+ > ^ <
+"""
+
+
+print(cat)
+
+
+from ast import Import
+import time
+import os
+from turtle import home
+
+
+frames = [
+    r"""
+ /\_/\  
+( o.o )
+ > ^ <  """,
+
+
+    r"""
+ /\_/\  
+( o.o )
+  > ^ < """,
+]
+
+for i in range(20):
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(frames[i % 2])
+    time.sleep(0.5)
+
+#######################################################################
+
+import time
+
+# Define the main lyrics as a list
+lyrics = [
+    "Buy it", "Use it", "Break it", "Fix it",
+    "Trash it", "Change it", "Mail it", "Upgrade it",
+    "Charge it", "Point it", "Zoom it", "Press it",
+    "Snap it", "Work it", "Quick, Erase it",
+    "Write it", "Cut it", "Paste it", "Save it",
+    "Load it", "Check it", "Quick, Rewrite it",
+    "Plug it", "Play it", "Burn it", "Rip it",
+    "Drag it", "drop"
+]
+
+
+# Play the lyrics once with delay
+import time
+
+
+for line in lyrics:
+    print(line)
+    time.sleep(1)# 400ms delay
+
+############################################################################
+#######################################################################
+
+print("................Loading.....................")
+time.sleep(0.1)
+
+print("................Downloading ................")
+time.sleep(0.1)
+
+import time
+
+
+lyrics  = [
+"Technologic","Technologic","Technologic","Technologic", "Technologic", "Technologic","Technologic","Technologic","Technologic", "Technologic", "Technologic","Technologic","Technologic","Technologic", "Technologic", "Technologic","Technologic","Technologic","Technologic", "Technologic",
+"Technologic","Technologic","Technologic","Technologic", "Technologic", "Technologic","Technologic","Technologic","Technologic", "Technologic", "Technologic","Technologic","Technologic","Technologic", "Technologic", "Technologic","Technologic","Technologic","Technologic", "Technologic",
+]
+
+# Play the lyrics once with delay
+import time
+
+
+for line in lyrics:
+    print(line)
+    time.sleep(2) # 400ms delay
+
+
+print("Greetings Let's take a Journey")
+print("Would you like to open Technologic?")
+
+###############################################################################################################################################################################################
+Homer = r"""
+     _____
+    /     \
+   | () () |
+    \  ^  /
+     |||||
+     |||||
+"""
+
+print(Homer)
+
+
+username = input("Please enter your name: ")
+
+
+user_age = int(input("Hello, " + username + "! How old are you? "))
+
+#######################################################################
+
+import time
+import os
+import random
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def fake_hack():
+    clear()
+    print("Initializing hacking sequence...\n")
+    time.sleep(1)
+
+    # Fake IP hacking
+    for _ in range(10):
+        ip = ".".join(str(random.randint(1, 255)) for _ in range(4))
+        print(f"Hacking into {ip} ...")
+        time.sleep(0.3)
+
+    # Fake firewall and access
+    print("\nBypassing firewall...")
+    time.sleep(1)
+    print("Access granted.\n")
+    time.sleep(1)
+
+    # Fake file download
+    for _ in range(5):
+        percent = random.randint(10, 100)
+        print(f"Downloading confidential files... ({percent}%)")
+        time.sleep(0.5)
+
+    # Freddy horror moment
+    print("\nInjecting Freddy Krueger virus...")
+    time.sleep(1.5)
+
+    print("\n🔥 SYSTEM OVERRIDE INITIATED 🔥")
+    time.sleep(2)
+
+    print("\n🩸 This is my world, and you can't ever leave. 🩸")
+    time.sleep(2)
+
+    print("\nYOU HAVE BEEN HACKED!\n")
+    print("💀💀💀💀💀💀💀💀💀💀💀\n")
+    time.sleep(5)
+
+    # Prank reveal
+    print("Just kidding 😄 — this was a prank.")
+    input("Press Enter to exit...")
+
+# Run the prank
+fake_hack()
+
+message = ["Buy it", "Use it", "Break it", "Fix it",
+"Trash it", "Change it", "Mail it", "Upgrade it",
+"Charge it", "Point it", "Zoom it", "Press it",
+"Snap it", "Work it", "Quick, Erase it",
+"Write it", "Cut it", "Paste it", "Save it",
+"Load it", "Check it", "Quick, Rewrite it",
+"Plug it", "Play it", "Burn it", "Rip it",
+"Drag it", "Drop it"
+]
+
+repeated_message = message * 100
+print(repeated_message)
+
+print("\nNow I'm playing with power! 🎮")
+
