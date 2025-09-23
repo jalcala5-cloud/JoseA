fd2c4db70c8b4940361bfcbb753943df6135a66b
Joe <jalcala5@mail.pima.edu>
2025-09-23 10:36:14 -0700

    Add files via upload

---
+++ New Freddy Function.py
@@ -0,0 +1,23 @@
+
+
+def greet_user(name):
+    return "Hello, " + name + "! Nice to meet you."
+
+message = greet_user("Freddy")
+print("This is my world. And you can't ever leave." + "Now I'm playing with power!" + message)
+
+def double_amount(amount):
+    double = amount * 5
+    print('Twice that amount is ' + str(double))
+    return double
+
+num = int(input('Enter a number:\n'))
+print(double_amount(num))
+
+
+
+
+
+
+
+
