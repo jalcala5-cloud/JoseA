ab6f703bbd5f35822f96a187832d721502cf45a7
Joe <jalcala5@mail.pima.edu>
2025-11-20 12:12:59 -0700

    Add files via upload

---
+++ csvLab.ps1
@@ -0,0 +1,15 @@
+﻿# Import the employees.csv file
+$employees = Import-Csv -Path "C:\powershell\employees.csv"
+
+# Create a new array for the phone information
+$phoneList = $employees | ForEach-Object {
+    # Select only the properties we need
+    [PSCustomObject]@{
+        LastName  = $_.LastName
+        FirstName = $_.FirstName
+        Phone     = $_.PhoneNumber
+    }
+}
+
+# Export the new array to phone.csv
+$phoneList | Export-Csv -Path "C:\powershell\phone.csv" -NoTypeInformation
